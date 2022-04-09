16c3c473a1d149e80611bb18b25bc4042d5725b6
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-04-09 18:39:22 +0200

    💃 Concentric Rings initial animation

--- src/concentricRings.ts
+++ src/concentricRings.ts
@@ -1,19 +1,28 @@
 import h from "hyperscript";
 
 import { Circle, Line, SVGBuilder } from "./utils/svg";
+import {
+  get12HourRatio,
+  getMillisRatio,
+  getMinuteRatio,
+  getSecondRatio,
+} from "./utils/time";
 
 const createHourMarkers = () =>
   new Array(12).fill(null).map((_, i) => {
     const ratio = i / 12;
     const x = Math.sin(ratio * Math.PI * 2);
     const y = -Math.cos(ratio * Math.PI * 2);
-    const length = i % 3 === 0 ? 10 : 5;
     return Line({
       x1: 95 * x,
       y1: 95 * y,
-      x2: (95 - length) * x,
-      y2: (95 - length) * y,
-      strokeWidth: 3,
+      x2: 85 * x,
+      y2: 85 * y,
+      color: i === 0 ? "red" : "black",
+      rotationOpts: {
+        duration: "12h",
+        startOffset: get12HourRatio(new Date()),
+      },
     });
   });
 
@@ -29,6 +38,11 @@ const createMinuteMarkers = () =>
       x2: (75 - length) * x,
       y2: (75 - length) * y,
       strokeWidth: 2,
+      color: i === 0 ? "red" : "black",
+      rotationOpts: {
+        duration: "1h",
+        startOffset: getMinuteRatio(new Date()),
+      },
     });
   });
 
@@ -44,6 +58,30 @@ const createSecondMarkers = () =>
       x2: (55 - length) * x,
       y2: (55 - length) * y,
       strokeWidth: 2,
+      color: i === 0 ? "red" : "black",
+      rotationOpts: {
+        duration: "60s",
+        startOffset: getSecondRatio(new Date()),
+      },
+    });
+  });
+
+const createMillisecondMarkers = () =>
+  new Array(1).fill(null).map((_, i) => {
+    const ratio = i / 1;
+    const x = Math.sin(ratio * Math.PI * 2);
+    const y = -Math.cos(ratio * Math.PI * 2);
+    return Line({
+      x1: 35 * x,
+      y1: 35 * y,
+      x2: 30 * x,
+      y2: 30 * y,
+      strokeWidth: 5,
+      color: i === 0 ? "red" : "black",
+      rotationOpts: {
+        duration: "1s",
+        startOffset: getMillisRatio(new Date()),
+      },
     });
   });
 
@@ -51,20 +89,22 @@ export const ConcentricRings = () => {
   const hourCircle = Circle();
   const minuteCircle = Circle({ r: 75 });
   const secondCircle = Circle({ r: 55 });
+  const millisecondCircle = Circle({ r: 35 });
 
   const hourMarkers = createHourMarkers();
   const minuteMarkers = createMinuteMarkers();
   const secondMarkers = createSecondMarkers();
-
-  // TODO animate the circles
+  const millisecondMarkers = createMillisecondMarkers();
 
   const svg = SVGBuilder()
-    .with(hourCircle)
-    .with(minuteCircle)
-    .with(secondCircle)
     .with(...hourMarkers)
     .with(...minuteMarkers)
     .with(...secondMarkers)
+    .with(...millisecondMarkers)
+    .with(hourCircle)
+    .with(minuteCircle)
+    .with(secondCircle)
+    .with(millisecondCircle)
     .build();
 
   return h(
@@ -72,7 +112,7 @@ export const ConcentricRings = () => {
     h("h2", "Concentric Rings"),
     h(
       "p",
-      "Concentric rings rotate around a common centre. The outer most ring rotates once every 12 hours. The middle ring once every hour. The innermost ring once every minute."
+      "Concentric rings rotate around a common centre. The outer most ring rotates once every 12 hours. The second ring once every hour. The third ring once every minute. The innermost ring once every second. Red markers correspond to the zero on each dial."
     ),
     svg
   );
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,43 +1,47 @@
-const getTimeRatio = (
-  time: Partial<
-    Pick<Date, "getDay" | "getHours" | "getMinutes" | "getSeconds">
-  >,
-  modSeconds: number
-) => {
-  const day = time.getDay?.() ?? 0;
-  const hours = time.getHours?.() ?? 0;
-  const minutes = time.getMinutes?.() ?? 0;
-  const seconds = time.getSeconds?.() ?? 0;
+const createTimeRatioGetter =
+  (modSeconds: number) =>
+  (
+    time: Partial<
+      Pick<
+        Date,
+        "getDay" | "getHours" | "getMinutes" | "getSeconds" | "getMilliseconds"
+      >
+    >
+  ) => {
+    const day = time.getDay?.() ?? 0;
+    const hours = time.getHours?.() ?? 0;
+    const minutes = time.getMinutes?.() ?? 0;
+    const seconds = time.getSeconds?.() ?? 0;
+    const millis = time.getMilliseconds?.() ?? 0;
 
-  const totalSeconds =
-    24 * 60 * 60 * day + 60 * 60 * hours + 60 * minutes + seconds;
+    const totalSeconds =
+      (24 * 60 * 60 * 1000 * day +
+        60 * 60 * 1000 * hours +
+        60 * 1000 * minutes +
+        1000 * seconds +
+        millis) /
+      1000;
 
-  return (totalSeconds % modSeconds) / modSeconds;
-};
+    return (totalSeconds % modSeconds) / modSeconds;
+  };
 
-export const getDayRatio = (
-  time: Pick<Date, "getDay" | "getHours" | "getMinutes">
-) => getTimeRatio(time, 7 * 24 * 60 * 60);
+export const getDayRatio = createTimeRatioGetter(7 * 24 * 60 * 60);
 
 /**
  * Given a timestamp, returns the ratio of the hours in the day
  */
-export const get24HourRatio = (
-  time: Pick<Date, "getSeconds" | "getMinutes" | "getHours">
-) => getTimeRatio(time, 24 * 60 * 60);
+export const get24HourRatio = createTimeRatioGetter(24 * 60 * 60);
 
 /**
  * Given a timestamp, returns the ratio of the hours in the day mod 12
  */
-export const get12HourRatio = (
-  time: Pick<Date, "getSeconds" | "getMinutes" | "getHours">
-) => getTimeRatio(time, 12 * 60 * 60);
+export const get12HourRatio = createTimeRatioGetter(12 * 60 * 60);
 
 /**
  * Given a timestamp, returns the ratio of the minutes relative to the hour
  */
-export const getMinuteRatio = (time: Pick<Date, "getMinutes" | "getSeconds">) =>
-  getTimeRatio(time, 60 * 60);
+export const getMinuteRatio = createTimeRatioGetter(60 * 60);
 
-export const getSecondRatio = (time: Pick<Date, "getSeconds">) =>
-  getTimeRatio(time, 60);
+export const getSecondRatio = createTimeRatioGetter(60);
+
+export const getMillisRatio = createTimeRatioGetter(1);
